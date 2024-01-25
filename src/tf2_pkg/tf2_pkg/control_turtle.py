9661e6d393da9933db8aa23d799d6a60f7d25794
TechnoTeeHee <tahakaleemmalhi@gmail.com>
2024-01-24 20:29:55 -0600

    lab9 done

--- src/tf2_pkg/tf2_pkg/control_turtle.py
+++ src/tf2_pkg/tf2_pkg/control_turtle.py
@@ -11,9 +11,10 @@ import tf2_ros
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 import numpy as np
+import time
 
 Kv = 2.0
-Ka = 7.0
+Ka = 6.0
 
 class TurtlesimMoveNode(Node):
     def __init__(self):
@@ -22,10 +23,9 @@ class TurtlesimMoveNode(Node):
         self.declare_parameter("ka", Ka)
         self.kv = self.get_parameter("kv").value
         self.ka = self.get_parameter("ka").value
-        self.goal_x = 0
-        self.goal_y = 0
-        self.pose_ = None
-        self.pose_subscriber_ = self.create_subscription(Pose, "turtle1/pose", self.get_turtle_pose, 10)
+        self.goal_x = None
+        self.goal_y = None
+        self.index = 0
         self.alive_turtles = None
         self.alive_turtles_subscriber_ = self.create_subscription(TurtleArray, "alive_turtles", self.get_alive_turtles, 10)
         self.cmd_vel_publisher_ = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
@@ -33,84 +33,66 @@ class TurtlesimMoveNode(Node):
         #self.get_logger().info('The control monitor has started.')
         self.tf_buffer_ = Buffer()
         self.tf_listener_ = TransformListener(self.tf_buffer_, self)
-        self.timer = self.create_timer(1.0, self.listen_tf_message)
+        time.sleep(1.0)
+        self.timer = self.create_timer(0.01, self.listen_tf_message)
+        
         
     def listen_tf_message(self):
         if self.alive_turtles == None or len(self.alive_turtles.turtles) == 0:
             return
-    
         origin_frame = "world"
         dest_frame = "turtle1"
         try:
-            t = self.tf_buffer_.lookup_transform(origin_frame, dest_frame,rclpy.time.Time())
+            t = self.tf_buffer_.lookup_transform(origin_frame, dest_frame, rclpy.time.Time())
             q = [t.transform.rotation.x, t.transform.rotation.y,
-            t.transform.rotation.z, t.transform.rotation.w]
-            e = quaternion_to_euler_degrees(q)        
-            self.get_logger().info(f"{origin_frame} to {dest_frame}: x = {round(t.transform.translation.x,1)}, y = {t.transform.translation.y}, yaw = {round(e[2],1)}")
-            self.get_logger().info(str(self.pose_.x))
+            t.transform.rotation.z, t.transform.rotation.w]     
             self.x = round(t.transform.translation.x,1)
             self.y = round(t.transform.translation.y,1)
-            self.yaw = round(e[2],1)
+            self.theta = t.transform.rotation.z*math.pi
         except tf2_ros.TransformException as ex:
             self.get_logger().info(f'Could not transform {origin_frame} to {dest_frame}: {ex}')
-            
-        origin_frame = "world"
-        dest_frame = "turtle1"
+        
+        self.index = 0
+        closest = -1
         try:
-            t = self.tf_buffer_.lookup_transform(origin_frame, dest_frame,rclpy.time.Time())
-            q = [t.transform.rotation.x, t.transform.rotation.y,
-            t.transform.rotation.z, t.transform.rotation.w]
-            e = quaternion_to_euler_degrees(q)        
-            self.get_logger().info(f"{origin_frame} to {dest_frame}: x = {round(t.transform.translation.x,1)}, y = {t.transform.translation.y}, yaw = {round(e[2],1)}")
-            self.get_logger().info(str(self.pose_.x))
-            self.x = round(t.transform.translation.x,1)
-            self.y = round(t.transform.translation.y,1)
-            self.yaw = round(e[2],1)
+            for i in range(len(self.alive_turtles.turtles)):
+                n = self.tf_buffer_.lookup_transform("turtle1", str(self.alive_turtles.turtles[i].name), rclpy.time.Time())
+                x = n.transform.translation.x
+                y = n.transform.translation.y
+                distance = math.sqrt(x**2 + y**2)
+                if closest == -1 or distance < closest:
+                    self.index = i
+                    closest = distance
+            g = self.tf_buffer_.lookup_transform("world", self.alive_turtles.turtles[self.index].name, rclpy.time.Time())       
+            self.goal_x = g.transform.translation.x 
+            self.goal_y = g.transform.translation.y
         except tf2_ros.TransformException as ex:
-            self.get_logger().info(f'Could not transform {origin_frame} to {dest_frame}: {ex}')
-            
-
+            return
 
     def get_alive_turtles(self, msg):
         self.alive_turtles = msg
 
-    def get_turtle_pose(self, msg):
-        self.pose_ = msg
-
     def turtle_move(self):
-        if self.pose_ == None:
+        if self.alive_turtles == None or len(self.alive_turtles.turtles) == 0 or self.goal_x == None:
             return
-        if self.alive_turtles == None or len(self.alive_turtles.turtles) == 0:
-            return
-        
-        # Determine closest turtle
-        index = 0
-        closest = -1
-        for i in range(len(self.alive_turtles.turtles)):
-            x = self.alive_turtles.turtles[i].x - self.pose_.x
-            y = self.alive_turtles.turtles[i].y - self.pose_.y
-            distance = math.sqrt(x**2 + y**2)
-            if closest == -1 or distance < closest:
-                index = i
-                closest = distance
-        self.goal_x = self.alive_turtles.turtles[index].x
-        self.goal_y = self.alive_turtles.turtles[index].y
-        xdist_to_goal = self.goal_x - self.pose_.x
-        ydist_to_goal = self.goal_y - self.pose_.y
+
+        xdist_to_goal = self.goal_x - self.x
+        ydist_to_goal = self.goal_y - self.y
         dist_to_goal = math.sqrt(xdist_to_goal**2 + ydist_to_goal**2)
         vel_msg = Twist()
         
-        if dist_to_goal > 0.2:
+        if dist_to_goal > 0.5:
             vel_msg.linear.x = self.kv * (dist_to_goal)
             goal_angle = math.atan2(ydist_to_goal, xdist_to_goal)
-            angle_error = goal_angle - self.pose_.theta
+            angle_error = goal_angle - self.theta
             angle_error = math.atan2(math.sin(angle_error),math.cos(angle_error))
             vel_msg.angular.z = self.ka * angle_error
         else:
             vel_msg.linear.x = 0.0
             vel_msg.angular.z = 0.0
-            self.capture_turtle(self.alive_turtles.turtles[index].name)
+            self.capture_turtle(str(self.alive_turtles.turtles[self.index].name))
             self.alive_turtles = None
+            self.goal_x = None
 
         self.cmd_vel_publisher_.publish(vel_msg)
 
@@ -134,19 +116,6 @@ class TurtlesimMoveNode(Node):
         except Exception as e:
             self.get_logger().error("Service call failed %r" % (e,))
             
-def quaternion_to_euler_degrees(q):
-    e = np.empty((3, ))
-    t0 = 2.0 * (q[3]*q[0] + q[1]*q[2])
-    t1 = 1.0 - 2.0 * (q[0]*q[0] + q[1]*q[1])
-    e[0] = np.degrees(np.arctan2(t0,t1))
-    t2 = 2.0 * (q[3]*q[1] - q[2]*q[0])
-    t2 = 1.0 if t2 > 1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    e[1] = np.degrees(np.arcsin(t2))
-    t3 = 2.0 * (q[3]*q[2] + q[0]*q[1])
-    t4 = 1.0 - 2.0 * (q[1]*q[1] + q[2]*q[2])
-    e[2] = np.degrees(np.arctan2(t3,t4))
-    return e
 
 def main(args=None):
     rclpy.init(args=args)
