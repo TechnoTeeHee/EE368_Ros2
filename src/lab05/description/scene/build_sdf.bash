c4ec3497fb4468d5f3d06ad8bd1f313e54de3dd5
TechnoTeeHee <tahakaleemmalhi@gmail.com>
2024-03-13 00:29:58 -0600

    ex 5.2 done

---
+++ src/lab05/description/scene/build_sdf.bash
@@ -0,0 +1,2 @@
+xacro scene.urdf.xacro > scene.urdf
+ign sdf -p scene.urdf > scene.sdf
--- src/pylab05/pylab05/ex52_control.py
+++ src/pylab05/pylab05/ex52_control.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
-
+import numpy as np
 from .point_cloud2 import *
 
 class ControlNode(Node):
@@ -18,7 +18,42 @@ class ControlNode(Node):
         data = list(gen)
         width = msg.width
         height = msg.height
-        
+        top = data[int(width/2)][0]
+
+        if (top == np.inf):
+            top_clear = 1
+            reset = 0
+        else:
+            top_clear = 0
+            reset = 1
+
+        arr = []
+
+        for i in range(1,height-1):
+            x = data[i*width + int(width/2)][0] 
+            z = (data[i*width + int(width/2)][2]) + 0.45
+
+            if (round(z, 1) != 0):
+                if (reset == 1 and x == np.inf):
+                    reset = 0
+                    arr.append(data[(i-1)*width + int(width/2)][2] + 0.45)
+                elif(reset == 0 and x != np.inf):
+                    reset = 1
+                    arr.append(z)
+
+        if (len(arr) == 0):
+            if (top_clear == 1):
+                self.get_logger().info('All clear')
+            else:
+                self.get_logger().info("Object dead ahead, distance = " + str(top))
+        elif (len(arr) == 1):
+            if(top_clear == 1):
+                self.get_logger().info('Bottom blocked ' + str(arr[0]))
+            else:
+                self.get_logger().info('Door height ' + str(arr[0]))
+        elif (len(arr) == 2):
+            self.get_logger().info('Opening z ' + str(arr[0]) + ' to ' + str(arr[1]))
+
         # "data" is a linear array of width x height "points"
         # For each point, x=point[0], y=point[1], z=point[2]
         # ...
